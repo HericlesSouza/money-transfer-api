2293a9acb098e1a0123e66541c5938ccd8dd6ab8
Hericles Souza <hericles_003@hotmail.com>
2025-06-20 05:47:01 -0300

    feat(Users): create user entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,28 @@
+import { Exclude } from 'class-transformer';
+import { Transfer } from 'src/transfer/entities/transfer.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Exclude()
+  @Column()
+  password: string;
+
+  @Column({ type: 'date' })
+  birthdate: Date;
+
+  @Column({ type: 'numeric', precision: 12, scale: 2, default: 0 })
+  balance: string;
+
+  @OneToMany(() => Transfer, (t) => t.from)
+  outgoing: Transfer[];
+
+  @OneToMany(() => Transfer, (t) => t.to)
+  incoming: Transfer[];
+}
