f947febd93b7e172d9f5058162d824bce3ac1dda
Hericles Souza <hericles_003@hotmail.com>
2025-06-20 18:52:39 -0300

    refactor(config): change lenght validation from jwt_secret

--- src/config/validation.ts
+++ src/config/validation.ts
@@ -3,6 +3,6 @@ import * as Joi from 'joi';
 export const validationSchema = Joi.object({
   API_PORT: Joi.number().default(3000),
   DATABASE_PATH: Joi.string().default('./database/db.sqlite'),
-  JWT_SECRET: Joi.string().min(32).required(),
+  JWT_SECRET: Joi.string().min(5).required(),
   JWT_EXPIRES_IN: Joi.string().default('3600s'),
 });
